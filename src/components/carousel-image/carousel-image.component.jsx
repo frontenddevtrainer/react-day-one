05dc959b3e8644ba10e969091e6043154ec11a0c
Trainer <admin@example.com>
2025-04-06 17:43:54 +0100

    Component Properties.

--- src/components/carousel-image/carousel-image.component.jsx
+++ src/components/carousel-image/carousel-image.component.jsx
@@ -1,7 +1,13 @@
-function CarouselImage() {
-    return <div style={{ backgroundColor: "red", height: "300px", width: "100%" }}>
+//Component Property
 
-    </div>
+function CarouselImage(props) {
+    // props = typeof Object { bgcolor: string }
+    // props.someCustomKey = value;
+
+    const bgcolor = props.bgcolor;
+    const opacity = props.opacity || 1;
+
+    return <div style={{ backgroundColor: bgcolor, height: "300px", width: "100%", opacity: opacity }}></div>
 }
 
 export default CarouselImage;
--- src/components/homepage-gallery/homepage-gallery.component.jsx
+++ src/components/homepage-gallery/homepage-gallery.component.jsx
@@ -4,21 +4,21 @@ import CarouselImage from '../carousel-image/carousel-image.component';
 function HomepageGallery() {
     return <Carousel>
         <Carousel.Item>
-            <CarouselImage text="First slide" />
+            <CarouselImage bgcolor="blue" text="First slide" />
             <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <CarouselImage text="Second slide" />
+            <CarouselImage bgcolor="red" opacity={.5} text="Second slide" />
             <Carousel.Caption>
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <CarouselImage text="Third slide" />
+            <CarouselImage bgcolor="yellow" opacity={.8} text="Third slide" />
             <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>
