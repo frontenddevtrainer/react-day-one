4792df66fd93d58037f567d3a9932a58c406fc88
Trainer <admin@example.com>
2025-04-06 18:46:19 +0100

    Conditions.

--- src/components/post-listing/post-listing.component.jsx
+++ src/components/post-listing/post-listing.component.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
-async function getAPIData(url){
+async function getAPIData(url) {
     const response = await fetch(url);
     return await response.json();
 }
 
-function PostListing(){
+function PostListing() {
 
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState(null);
 
     const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
-    useEffect(function(){
+    useEffect(function () {
 
-        async function callApi(){
+        async function callApi() {
             const postsData = await getAPIData(apiUrl);
             setPosts(postsData)
         }
@@ -23,12 +23,20 @@ function PostListing(){
     }, []);
     // Lifecycle hook componentDidMount;
 
-    
+
+    if (!posts) {
+        return <div style={{ backgroundColor: "green" }}>Loading..</div>
+    }
+
 
     return <ul>
         {
-            posts.map(function(item){
-                return <li onClick={function(){ alert(item.title) }}>{item.title}</li>
+            posts.map(function (item) {
+                return <li onClick={function () { alert(item.title) }}>
+                    <span style={{ fontSize: "24px" }}>{item.id}</span>
+                    {item.title}
+                    {item.id % 2 === 1 ? <span style={{ color: "blue" }}>{item.body}</span> : null}
+                </li>
             })
         }
     </ul>
